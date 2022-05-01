16d6bf34bebc880d82ef5c6d97e4aae9908a4e32
eddyhood <eddy@ignitespot.com>
2022-05-01 06:54:22 -0600

    Remove periods before image

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const scheme = (window.matchMedia('(prefers-color-scheme: dark)').matches)
 let logo = document.getElementById('header-logo');
 
 if(scheme == true) {
-    logo.src='../images/Dollarwich - Dark-no-text.png';
+    logo.src='/images/Dollarwich - Dark-no-text.png';
 }
